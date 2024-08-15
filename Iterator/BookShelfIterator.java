e9915b4963c54fb9da1a18a992d839c4fdf3a606
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2024-08-15 23:07:37 +0900

    イテレータパターンの実装

---
+++ Iterator/BookShelfIterator.java
@@ -0,0 +1,32 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class BookShelfIterator implements Iterator<Book> {
+    private BookShelf bookShelf;
+    private int index;
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Book next() {
+        if (index < bookShelf.getLength()) {
+            Book book = bookShelf.getBookAt(index);
+            index++;
+            return book;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+}
