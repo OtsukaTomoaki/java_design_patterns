632984c9db9c2619e23d6b8e3da7988b2b3bf3e6
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2024-08-16 00:15:06 +0900

    ArrayListVerを作成

--- Iterator/BookShelf.java
+++ Iterator/BookShelf.java
@@ -1,27 +1,26 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class BookShelf implements Iterable<Book> {
-    private Book[] books;
-    private int last = 0;
+    private ArrayList<Book> books;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf() {
+        this.books = new ArrayList<>();
     }
 
     public Book getBookAt(int index) {
-        return books[index];
+        return books.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        this.books.add(book);
     }
 
     public int getLength() {
-        return last;
+        return books.size();
     }
 
     public Iterator<Book> iterator() {
-        return new BookShelfIterator(this);
+        return books.iterator();
     }
 }
