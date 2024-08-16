4a6311f23e1d10ce0c88a2da5166f518886706b2
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2024-08-16 19:01:33 +0900

    factorymethodパターン

---
+++ FactoryMethod/idcard/IDCardFactory.java
@@ -0,0 +1,15 @@
+package FactoryMethod.idcard;
+
+import FactoryMethod.framework.Product;
+import FactoryMethod.framework.Factory;
+
+public class IDCardFactory extends Factory {
+    @Override
+    protected Product createProduct(String owner) {
+        return new IDCard(owner);
+    }
+    @Override
+    protected void registerProduct(Product product) {
+        System.out.println(product + "のカードを登録します。");
+    }
+}
